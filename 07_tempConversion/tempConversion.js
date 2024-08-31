dc03ed7a75a41fecb2ffe50bf1e6d5dbab20a60c
tickytaby <riccardo.yin@outlook.com>
2024-08-31 17:15:12 +0200

    completed all exercises regarding Arrays & Loops

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,16 @@
-const convertToCelsius = function() {
+function roundTo(number) {
+  let rounded = Math.round(number * 10) / 10
+  return rounded
+}
+
+const convertToCelsius = function(far) {
+  c = 5/9*(far - 32)
+  return roundTo(c)
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(c) {
+  far = (9/5*c) + 32
+  return roundTo(far)
 };
 
 // Do not edit below this line
